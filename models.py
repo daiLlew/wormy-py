328532664b3bb946c4f106f3c94f90ad23338567
Dave Llewellyn <439837@gmail.com>
2018-06-29 10:26:05 +0100

    added collision detection for boundary and worm body also added apple consumption

--- models.py
+++ models.py
@@ -14,84 +14,42 @@ LEFT = "left"
 RIGHT = "right"
 
 
-class Canvas:
-    size = None
+class Game:
 
-    def __init__(self, size):
+    def __init__(self, display_surface, worm, size):
+        self.display_surface = display_surface
+        self.direction = DOWN
+
         self.size = size
-        self.rect = pygame.Rect(0, 0, self.size, self.size)
+        self.canvas_rect = pygame.Rect(0, 0, self.size, self.size)
 
+        self.worm = worm
+        self.worm.add(self.direction)
+        self.worm.add(self.direction)
 
-class Snake:
-    length = None
-    body = []
-    direction = UP
+        self.apples = []
+        self.apples.append(Apple())
 
-    def __init__(self, start_x, start_y):
-        self.length = 1
-        self.body.append(BodySegment(start_x, start_y))
-        self.ani_speed = ANIMATION_SPEED
+    def draw(self):
+        # draw the background
+        self.display_surface.fill(WHITE)
 
-        self.add()
-        self.add()
-
-    def draw(self, surface):
+        # draw the worm
         head = True
-        for segment in self.body:
+        for b in self.worm.body:
             if head:
-                pygame.draw.rect(surface, GREEN, segment.rect)
+                pygame.draw.rect(self.display_surface, GREEN, b.rect)
                 head = False
             else:
-                pygame.draw.rect(surface, RED, segment.rect)
-                pygame.draw.rect(surface, BLACK, segment.xy())
+                pygame.draw.rect(self.display_surface, RED, b.rect)
+                pygame.draw.rect(self.display_surface, BLACK, b.xy())
 
-    def add(self):
-        last = self.body[-1]
-        new_x = None
-        new_y = None
+        # draw any apples
+        for a in self.apples:
+            pygame.draw.rect(self.display_surface, BLACK, a.rect)
 
-        if self.direction == UP:
-            new_x = last.rect.x
-            new_y = last.rect.y + SEGMENT_SIZE
-        elif self.direction == DOWN:
-            new_x = last.rect.x
-            new_y = last.rect.y - SEGMENT_SIZE
-        elif self.direction == LEFT:
-            new_x = last.rect.x - SEGMENT_SIZE
-            new_y = last.rect.y
-        else:
-            new_x = last.rect.x + SEGMENT_SIZE
-            new_y = last.rect.y
-
-        print("current x", last.x_co, "current y", last.y_co)
-        print("new x", new_x, "new y", new_y)
-        self.body.append(BodySegment(new_x, new_y))
-
-    def move(self):
-        if self.ani_speed > 0:
-            self.ani_speed -= 1
-            return
-
-        old_head = self.body[0]
-
-        if self.direction == UP:
-            new_x = old_head.rect.x
-            new_y = old_head.rect.y - MOVEMENT
-        elif self.direction == DOWN:
-            new_x = old_head.rect.x
-            new_y = old_head.rect.y + MOVEMENT
-        elif self.direction == LEFT:
-            new_x = old_head.rect.x - MOVEMENT
-            new_y = old_head.rect.y
-        else:
-            new_x = old_head.rect.x + MOVEMENT
-            new_y = old_head.rect.y
-
-        self.body.pop()
-        new_head = BodySegment(new_x, new_y)
-        self.body.insert(0, new_head)
-
-        self.ani_speed = ANIMATION_SPEED
+    def animate(self):
+        self.worm.move(self.direction)
 
     def update_direction(self, event):
         if event.key == pygame.K_LEFT and self.direction is not RIGHT:
@@ -103,8 +61,9 @@ class Snake:
         if event.key == pygame.K_DOWN and self.direction is not UP:
             self.direction = DOWN
 
-    def check_collisions(self, canvas):
-        head = self.body[0]
+    def check_collisions(self):
+        head = self.worm.head
+        body = self.worm.body[1:]
 
         # First check for a boundary collision with the appropriate edge of the playing area
         if self.direction == UP:
@@ -116,15 +75,81 @@ class Snake:
         else:
             p = head.rect.midright
 
-        if not canvas.rect.collidepoint(p):
+        if not self.canvas_rect.collidepoint(p):
             return True
 
         # Next check that the head is not colliding with any part of the body
-        for b in self.body[1:]:
-            if head.rect.colliderect(b):
-                return True
+        if head.rect.collidelistall(body):
+            return True
 
         return False
+
+    def add_apple(self):
+        self.apples.append(Apple())
+
+    def check_apple_eaten(self):
+        for i in range(len(self.apples)):
+            if self.worm.head.rect.colliderect(self.apples[i]):
+                print("apple consumed")
+                self.worm.add(self.direction)
+                del self.apples[i]
+                break
+
+
+class Worm:
+    length = None
+    body = []
+
+    def __init__(self, start_x, start_y):
+        self.length = 1
+        self.ani_speed = ANIMATION_SPEED
+        self.body.append(BodySegment(start_x, start_y))
+        self.head = self.body[0]
+
+    def add(self, direction):
+        last = self.body[-1]
+
+        if direction == UP:
+            new_x = last.rect.x
+            new_y = last.rect.y + SEGMENT_SIZE
+        elif direction == DOWN:
+            new_x = last.rect.x
+            new_y = last.rect.y - SEGMENT_SIZE
+        elif direction == LEFT:
+            new_x = last.rect.x - SEGMENT_SIZE
+            new_y = last.rect.y
+        else:
+            new_x = last.rect.x + SEGMENT_SIZE
+            new_y = last.rect.y
+
+        self.body.append(BodySegment(new_x, new_y))
+
+    def move(self, direction):
+        if self.ani_speed > 0:
+            self.ani_speed -= 1
+            return
+
+        old_head = self.head
+
+        if direction == UP:
+            new_x = old_head.rect.x
+            new_y = old_head.rect.y - MOVEMENT
+        elif direction == DOWN:
+            new_x = old_head.rect.x
+            new_y = old_head.rect.y + MOVEMENT
+        elif direction == LEFT:
+            new_x = old_head.rect.x - MOVEMENT
+            new_y = old_head.rect.y
+        else:
+            new_x = old_head.rect.x + MOVEMENT
+            new_y = old_head.rect.y
+
+        self.body.pop()
+        new_head = BodySegment(new_x, new_y)
+        self.body.insert(0, new_head)
+        self.head = new_head
+
+        self.ani_speed = ANIMATION_SPEED
 
 
 class BodySegment:
@@ -143,4 +168,15 @@ class BodySegment:
         return pygame.Rect(self.x_co, self.y_co, 2, 2)
 
     def __str__(self):
-        return "x = " + self.x_co + ", y = " + self.y_co
+        return "x = " + str(self.x_co) + ", y = " + str(self.y_co)
+
+
+class Apple:
+    x_co = None
+    y_co = None
+
+    def __init__(self):
+        self.x_co = 120
+        self.y_co = 120
+        self.rect = pygame.Rect(self.x_co, self.y_co, SEGMENT_SIZE, SEGMENT_SIZE)
+
